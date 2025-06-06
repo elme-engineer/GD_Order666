989e283d01a8699fb68eac5c5ac0a1d749fdd9fa
swaggggg9000 <helenabessa2002@gmail.com>
2025-06-06 02:17:00 +0100

    milhos a crescer e scene2 completa

--- Order666/Assets/Scripts/BulletManager.cs
+++ Order666/Assets/Scripts/BulletManager.cs
@@ -1,21 +1,34 @@
-using System.Threading;
 using UnityEngine;
 
 public class BulletManager : MonoBehaviour
 {
-
-    [SerializeField] float timeToDestroy;
+    [SerializeField] float timeToDestroy = 5f;
     [HideInInspector] public WeaponManager wpManager;
+
+    private Rigidbody rb;
+    private bool isStuck = false;
+    private Vector3 initialScale;
+    private Vector3 targetScale;
+    private float growSpeed = 2f;
 
     void Start()
     {
-        Destroy(this.gameObject, timeToDestroy);
+        rb = GetComponent<Rigidbody>();
+
+        initialScale = transform.localScale;
+
+        // Very small final size
+        targetScale = new Vector3(0.03f, 0.03f, 0.03f);
+
+        Invoke(nameof(DestroyIfNotStuck), timeToDestroy);
     }
 
-    
     void Update()
     {
-
+        if (isStuck)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * growSpeed);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -26,6 +39,29 @@ public class BulletManager : MonoBehaviour
             hbManager.TakeDamage(wpManager.damage);
             Destroy(this.gameObject);
         }
-            
+        else if (collision.gameObject.CompareTag("Plant") && !isStuck)
+        {
+            StickToPlant(collision);
+        }
+    }
+
+    void StickToPlant(Collision collision)
+    {
+        isStuck = true;
+        rb.linearVelocity = Vector3.zero;
+        rb.isKinematic = true;
+
+        transform.SetParent(collision.transform);
+        transform.up = collision.contacts[0].normal;
+    }
+
+    void DestroyIfNotStuck()
+    {
+        if (!isStuck)
+        {
+            Destroy(gameObject);
+        }
     }
 }
+
+
